de10fe4c53771750238ca21a7ae2c1c0f72a36be
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-12-27 10:45:46 +0600

    Initial setup app.js except routing and error-handling

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+dotenv.config();
+
+//database-connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => console.log("Database connection successfully"))
+  .catch((err) => console.log(err));
+
+//Request Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//routing setup
+
+//Error handling
+
+//app listening
+app.listen(process.env.PORT, () => {
+  console.log(`app listening port ${process.env.PORT}`);
+});
